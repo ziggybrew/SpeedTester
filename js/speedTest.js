58a02d1cfb3f9a8f07a6dca1f8930b4b89b94e83
Zack Modica <zmodica@gmail.com>
2019-11-20 21:49:35 -0500

    changes towards timer

--- js/speedTest.js
+++ js/speedTest.js
@@ -1,5 +1,19 @@
 function timer() {
     //timer to be shown to user as they type
+    var StartTime= new Date();
+		function DisplayTime(){
+			var CurrentTime= new Date();
+			var TimeDiff=CurrentTime.getTime()-StartTime.getTime();
+			TimeDiff=Math.floor(TimeDiff/1000);
+			var ShowTime=(TimeDiff<10)?'0:0':'0:';
+			document.getElementById('timer').innerHTML=ShowTime+TimeDiff;
+		}
+		var StopTime=setInterval(DisplayTime, 1000);
+		function StopUpdatingTime(){
+			clearInterval(StopTime);
+			document.getElementById("userInput").disabled = true;
+		}
+		setTimeout(StopUpdatingTime, 60000);
 }
 
 function speedCal() {
